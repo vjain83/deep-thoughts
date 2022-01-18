75d820516fa13bd0ae9054d685b63e2dd9d60c1f
Varsha Jain <csvarshajain@gmail.com>
2022-01-18 15:09:02 -0500

    added reaction component

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -3,6 +3,8 @@ import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
 import { useParams } from 'react-router-dom';
 import ReactionList from '../components/ReactionList'
+import Auth from '../utils/auth';
+import ReactionForm from '../components/ReactionList';
 
 const SingleThought = (props) => {
 
@@ -32,6 +34,7 @@ const SingleThought = (props) => {
         </div>
       </div>
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   )
 };
