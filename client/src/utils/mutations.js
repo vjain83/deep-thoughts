14a48c1708513141eef5a4492ea3f623921978f0
Varsha Jain <csvarshajain@gmail.com>
2022-01-17 23:51:32 -0500

    added form for tought and add friend

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,16 @@ export const ADD_USER = gql`
     }
   }
 `;
+export const ADD_FRIEND = gql`
+  mutation addFriend($id: ID!) {
+    addFriend(friendId: $id) {
+      _id
+      username
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
